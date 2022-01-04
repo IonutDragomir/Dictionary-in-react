4724afdc9c27d3c8b55858ac40714281f43d2e8f
Ionut Marian <ionutdragomir1410@yahoo.com>
2022-01-04 20:39:07 +0200

    refactored code to use props and use state

--- src/content/dicitionary.js
+++
@@ -1,8 +0,0 @@
-export const DisplayWords = () => {
-    return (
-      <div className="Dictionary">
-        <p style={{ color: "#125f9b" }}> Words:</p>
-        <div id="ShowDictionary" style={{ color: "#125f9b" }}></div>
-      </div>
-    );
-  };
--- src/content/innerContainer/title.js
+++
@@ -1,7 +0,0 @@
-export const Title = () => {
-    return (
-      <div className="Title">
-        <p>Dictionary</p>
-      </div>
-    );
-  };
